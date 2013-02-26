9e581185315a2b0f76de3d7b4b070e57bf8e0bfd
Scott Greenwald <swgreen@mit.edu>
2013-02-26 12:50:46 -0500

    support for keypress events and dashboard counting

--- remoteSlideshowServer/README.md
+++ remoteSlideshowServer/README.md
@@ -7,3 +7,14 @@ Minimal node.js + socket.io app that shows how to create dashboard and view inst
 
 * Change the host url in index.html if you're not running it on localhost.
 
+# Event support
+
+* **keypress** broadcasts whatever data comes with it. The convention is
+  meant to be that the data has a "keycode" attribute.
+
+* **clientEvent** when a new client connects, it uses this event to say what
+  kind of a client it is. The server uses this event to tell clients about 
+  other clients -- for now, how many dashboards are connected.
+
+
+
--- remoteSlideshowServer/app.js
+++ remoteSlideshowServer/app.js
@@ -2,7 +2,19 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , sharedVar = 10 
-  , port = process.env.PORT || 80;
+  , port = process.env.PORT || 80
+  , dashboardsConnected = 0
+  , clientTypes = {};
+
+// magical shit from heroku docs
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
+io.configure(function () {
+  io.set("heartbeat interval", 1);
+});
 
 app.listen(port);
 console.log("Starting the app (id=2013-01-25T21:08:12.854Z).")
@@ -44,13 +56,89 @@ function decrement() {
   sharedVar -=1;
 }
 
+function broadcastClients(socket) {
+    var data = {clients:dashboardsConnected};
+    io.sockets.emit('clientEvent',data);
+}
+
+function serverLog(message) {
+  io.sockets.emit('serverLog',{'message':message});
+}
+
 io.sockets.on('connection', function (socket) {
+  io.sockets.emit('clientEvent',{dashboards:dashboardsConnected});
   function updateAllSockets() {
     socket.emit('updateSharedVariable',sharedVar);
     socket.broadcast.emit('updateSharedVariable', sharedVar);
   }
-  socket.on('upArrow', function() {socket.emit('previous slide'); socket.broadcast.emit('previous slide')});
+
+  // var clientTypeGet = '';
+  // socket.get('client', function(err,name){
+  //   clientTypeGet = name;
+  // });
+  
+  socket.on('serverLog', function(data) {serverLog(data.message)});
+  
+  // TODO: get rid of up and down, they're subsumed by 'keypress'
+  socket.on('upArrow', function() {io.sockets.emit('previous slide')});
   socket.on('downArrow', function() {socket.emit('next slide'); socket.broadcast.emit('next slide')});
-  //socket.on('upArrow', function() {increment(); updateAllSockets();});
-  //socket.on('downArrow',function() {decrement(); updateAllSockets();});
+
+  // whenever a keypress event comes it, broadcast it along with its data    
+  socket.on('keypress', function(data) {io.sockets.emit('keypress',data);});
+  // socket.on('keypress', function(data) {socket.emit('keypress',data); socket.broadcast.emit('keypress',data)});
+
+  socket.on('clientEvent', function(data) {
+
+    // 1. check clientType in order to adjust dashboardsConnected
+
+    var clientType = data.clientType;
+    if (clientType === 'dashboard') {
+      serverLog("A dashboard connected, and now there are "+dashboardsConnected+" dashboards connected.");
+      dashboardsConnected += 1;
+    }
+
+    io.sockets.emit('clientEvent',{dashboards:dashboardsConnected});
+
+    // 2. store type so it can be checked upon disconnect
+
+    // probably no point in using the 'set' function for this,
+    //   since I can't use 'get' after disconnect.
+    //   but for now it doesn't hurt.
+    socket.set('clientType', clientTypeSet, function(){
+      clientTypes[socket.id] = clientTypeSet;
+    });
+  });
+
+  socket.on('disconnect', function() {
+    if (clientTypes[socket.id] === 'dashboard') {
+      serverLog("A dashboard was disconnected");
+      dashboardsConnected -= 1;
+      serverLog("Now there are "+dashboardsConnected+" dashboards connected.");
+    } else {
+      serverLog("Some non-dashboard client was disconnected.");
+    };
+
+    io.sockets.emit('clientEvent',{dashboards:dashboardsConnected});
+
+  });
 });
+    
+//   socket.on('disconnect', function () {
+//     /*
+//     // var clientTypeGet = "";
+//     // socket.get('client', function(err,name){
+//     //   clientTypeGet = name;
+//     // });
+//     try {
+//       serverLog("Detected that a client was disconnected."+socket.id);
+//       var keys = "";
+//       for (var key in socket) {
+//         keys += key+"\n";
+//       }
+//       serverLog("The socket keys: "+keys); // JSON.stringify(socket.keys()));
+//       //serverLog("socket.options is "+socket.options);
+//     } catch(e) {
+//       serverLog("Perhaps socket.options is undefined. "+e);
+//       serverLog("Detected that a client was disconnected (again)."+socket.id);
+
+//     }
